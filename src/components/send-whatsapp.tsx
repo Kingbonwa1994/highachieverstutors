e2442ea0dd547026fea62b1c4db3618cba48f889
kingbonwa <kingbonwa@hotmail.co.za>
2024-05-08 13:50:37 +0200

    reciepient number updated

--- src/components/send-whatsapp.tsx
+++ src/components/send-whatsapp.tsx
@@ -3,7 +3,7 @@ export default function composeWhatsAppMessage(subject: string, topic:string) {
     const encodedMessage = encodeURIComponent(message); // Encode special characters
   
     // WhatsApp URL with pre-filled message and optional phone number (replace with your tutor's number)
-    const whatsappUrl = `https://wa.me/+1234567890?text=${encodedMessage}`; // Customize phone number
+    const whatsappUrl = `https://wa.me/+27742911384?text=${encodedMessage}`; // Customize phone number
   
     return whatsappUrl;
   }
